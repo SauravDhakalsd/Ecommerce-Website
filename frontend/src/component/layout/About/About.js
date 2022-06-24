4f59e6e5bcec1fdf26b010b1432f7634b901c58a
Saurav Dhakal <86107331+SauravDhakalsd@users.noreply.github.com>
2022-06-24 17:00:32 +0545

    Add files via upload

---
+++ frontend/src/component/layout/About/About.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./aboutSection.css";
+import { Button, Typography, Avatar } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+const About = () => {
+  const visitInstagram = () => {
+    window.location = "https://instagram.com/saurav";
+  };
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              src=""
+              alt="Founder"
+            />
+            <Typography>Saurav Dhakal</Typography>
+            <Button onClick={visitInstagram} color="primary">
+              Visit Instagram
+            </Button>
+            <span>
+              This is a wesbite made by saurav dhakal.
+            </span>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+            <a
+              href="https://www.youtube.com"
+              target="blank"
+            >
+              <YouTubeIcon className="youtubeSvgIcon" />
+            </a>
+
+            <a href="https://instagram.com" target="blank">
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
